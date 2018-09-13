2486c0bc9e3e9c88b83e3a1675dcd87a7d17e8a1
Tsepo SHATA <tshata@e5r5p4.wethinkcode.co.za>
2018-09-13 17:15:54 +0200

    Function to solve still missing

--- push_swap_al/srcs/main.c
+++ push_swap_al/srcs/main.c
@@ -6,8 +6,126 @@
 /*   By: tshata <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 17:24:01 by tshata            #+#    #+#             */
-/*   Updated: 2018/09/13 13:59:22 by tshata           ###   ########.fr       */
+/*   Updated: 2018/09/13 17:09:20 by tshata           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "includes/push_swap.h"
 
+int read_nbrs(char *str, t_stack *s_a)
+{
+    int         sign;
+    long int    nbr;
+
+    nbr = 0;
+    sign = 1;
+    while (*str)
+    {
+        if (*str == '-')
+            sign = -1;
+        else if (!ft_isdigit(*str))
+        {
+            ft_putstr("Error\n");
+            exit (0);
+        }
+        else if (ft_isdigit(*str))
+            nbr = (nbr * 10) + (*str - '0');
+        if (*str == ' ' || !(*(str + 1)))
+        {
+            nbr *= sign;
+            if (!is_valid(s_a, nbr) && nbr != 0)
+            {
+                ft_putstr("Error\n");
+                exit(0);
+            }
+            s_a->nbrs[s_a->current_size] = nbr;
+            s_a->current_size++;
+            nbr = 0;
+            sign = 1;
+        }
+        str++;
+    }
+    return (1);
+}
+
+int             fill_s_a(char **argv, int argc, t_stack *s_a)
+{
+    int         i;
+
+    i = 1;
+    while (i <= argc)
+    {
+        read_nbrs(argv[i], s_a);
+//      ft_putnbr(ft_atoi(argv[i]));
+        i++;
+	}
+    return (1);
+}
+
+void        init(t_stack *s_a, t_stack *s_b, int size, char **argv)
+{
+    int         nbr;
+    int         i;
+
+    i = 0;
+    nbr = 0;
+    while (i < size)
+    {
+        if (!count_nbrs(argv[i]))
+        {
+            ft_putstr("Error\n");
+            exit(0);
+        }
+        nbr += count_nbrs(argv[i]);
+    //  ft_putnbr(nbr);
+        i++;
+    }
+    s_a->max_size = nbr;
+    s_a->current_size = 0;
+    s_a->nbrs = (int*)malloc(sizeof(int) * nbr);
+    s_b->max_size = nbr;
+    s_b->current_size = 0;
+    s_b->nbrs = (int*)malloc(sizeof(int) * nbr);
+}
+
+int         handle_input(char *line, t_stack *s_a, t_stack *s_b)
+{
+    while ((get_next_line(0, &line) == 1))
+    {
+        //exec_inst(line, s_a, s_b);
+        if (!exec_inst(line, s_a, s_b))
+        {
+            ft_putstr("Error\n");
+            exit(0);
+        }
+        free(line);
+    }
+    return (1);
+}
+
+int             main(int argc, char **argv)
+{
+    t_stack     s_a;
+    t_stack     s_b;
+    int         size;
+    char        *line;
+
+    size = 0;
+    size = argc - 1;
+    if (argc > 1)
+    {
+        init(&s_a, &s_b, size, argv);
+        line = NULL;
+        if (fill_s_a(argv, size, &s_a))
+        //    solve(line, &s_a, &s_b);
+		//    function to run push_swap comes here
+        if (is_sorted(s_a.nbrs, s_a.current_size) && (s_b.current_size == 0))
+            ft_putstr("OK\n");
+        else if (!is_sorted(s_a.nbrs, s_a.current_size))
+            ft_putstr("KO\n");
+        else
+            ft_putstr("Error\n");
+    free(s_a.nbrs);
+    free(s_b.nbrs);
+    }
+}
